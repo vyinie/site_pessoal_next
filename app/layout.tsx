64d810af7a9e8fe28256362a2ab00bd833ce7db2
vyinie <marcusvyinicius@gmail.com>
2023-09-30 17:50:50 -0300

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import './globals.css'
-import type { Metadata } from 'next'
+import "./globals.css";
+import type { Metadata } from "next";
 export const metadata: Metadata = {
-  title: 'Marcus Xavier',
-  
-}
+  title: "Marcus Xavier",
+  icons: "_images/home/homeFavIcon.ico",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-BR">
       <body className={``}>{children}</body>
     </html>
-  )
+  );
 }
